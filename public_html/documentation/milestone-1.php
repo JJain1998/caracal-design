be622cb2c26fe1a205aa41430ca02c54735f9109
Jack Jain <cirocelot@gmail.com>
2018-10-16 16:30:07 -0600

    added directories, milestone-1.php, and basic html framework

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Caracal Design</title>
+	</head>
+	<body>
+		<h1>Purpose</h1>
+		<h3>I will have a professional environment to showcase my projects and reach prospective freelance clients/employers.</h3>
+		<h1>Audience</h1>
+		<h3>This site will be viewed by other developers as well as clients searching for professional full-stack development services.</h3>
+		<h1>Goal</h1>
+		<h3>To serve as a gallery of my work in order to attract freelance clients, or full-time employment.</h3>
+	</body>
+</html>
