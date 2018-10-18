302086e27cb6453e14bf85591ff2a70cc3b8eb47
Jack Jain <cirocelot@gmail.com>
2018-10-17 21:04:34 -0600

    added persona/use case

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -11,5 +11,16 @@
 		<h3>This site will be viewed by other developers as well as clients searching for professional full-stack development services.</h3>
 		<h1>Goal</h1>
 		<h3>To serve as a gallery of my work in order to attract freelance clients, or full-time employment.</h3>
+		<h1>Persona</h1>
+		<p>
+			<ul>
+			<li><strong>Name:</strong>Bertram Quillman</li>
+			<li><strong>Age:</strong>33</li>
+			<li><strong>Occupation:</strong>Small business owner seeking professional website development.</li>
+			<li><strong>Tech:</strong>iPhone X, Macbook Air 13 inch, Yosemite OS</li>
+			<li><strong>Behavior/Attitude:</strong>He is a no-nonsense man, with little patience for tomfoolery. He respects professionalism,
+			despite not being completely up on the newer technologies. He wants great customer service, and expects exceptional quality. He is
+			browsing this site to determine if the sites showcased live up to his expectations of quality.</li>
+		</ul>
 	</body>
 </html>
