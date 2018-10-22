2d5a050f42d741ca5dac86ffde5ee692ca4c7fb3
Jack Jain <cirocelot@gmail.com>
2018-10-22 09:07:01 -0600

    created user story

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -24,5 +24,43 @@
 			despite not being completely up on the newer technologies. He wants great customer service, and expects exceptional quality. He is
 			browsing this site to determine if the sites showcased live up to his expectations of quality.</li>
 		</ul>
+		<h2>Frustrations</h2>
+		<ul>
+			<li>Lack of professional developers who don't have a portfolio demonstrating their technical skills</li>
+			<li>Difficulty finding developers who can communicate outside of the tech realm</li>
+		</ul>
+		<hr>
+		<h2>User Story</h2>
+		<p>Bertram would like to easily and conveniently view the work of a developer, to gauge the quality and determine a good candidate.</p>
+		<h2>Use Case</h2>
+		<p>
+			<strong>Title: </strong>Searching for quality developers to make a professional site.
+		</p>
+		<p>
+			<strong>Description: </strong>Bertram wants to find a good developer that can create a professional site for his business.
+		</p>
+		<p>
+			<strong>User's Name and Role: </strong> Bertram; Small business owner researching web developers for his professional site.
+		</p>
+		<p>
+			<strong>Usage preconditions: </strong>Must have a need for quality website development.
+		</p>
+		<p>
+			<strong>Usage postconditions: </strong>Will have an idea of the quality of content I can create, as well as easy means to contact me, or find my social media.
+		</p>
+
+		<p><strong>Interaction Flow: </strong>
+			<ul>
+			<li>Bertram enters caracal.design into the browser, the same site listed on the business card he was given</li>
+			<li>Server returns homepage of caracal.design</li>
+			<li>Bertram immediately sees an about me page, highlighting myself as a web developer</li>
+			<li>Bertram scrolls down the page, and finds links with thumbnails of my most recent projects</li>
+			<li>Bertram clicks on a link that catches his eye</li>
+			<li>The link takes him to one of my past sites</li>
+			<li>Bertram returns to caracal.design</li>
+			<li>Bertram enters his information into the "Contact me" form</li>
+			<li>Bertram's info is sent directly to me notifying my of his desire to hire a developer.</li>
+		</ul>
+		</p>
 	</body>
 </html>
