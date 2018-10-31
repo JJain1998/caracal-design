581a9473415370c778f6ec53044eb2b6dd2bcfeb
Jack Jain <cirocelot@gmail.com>
2018-10-31 16:38:47 -0600

    added milestone-2.php and mobile wireframes

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang = "eng">
+	<head>
+		<meta charset="UTF-8">
+		<title>Caracal Design Milestone 2</title>
+	</head>
+	<body>
+		<h1>Content Strategy</h1>
+		<hr>
+		<p><h2>This will be a one-page site, and will have these sections: </h2></p>
+		<h3>Welcome: </h3>Splash page displays my name as well as my company's name below. These will be overlaid on a
+		large jumbotron with a transitioning image.
+		<p>
+			<h3>Mission Statement:</h3> This section will give a brief description of what my professional goals are, and
+		will show the standards to which I hold myself and my company. There will be a professional tone, to attract
+		clients in a work environment, and convey my tone when handling work.
+		</p>
+		<p>
+			<h3>Past work: </h3> This section will have thumbnail images of past sites/work that I want highlighted. There will
+		 be a few (3-4) sites showcased, with short descriptions to the side of each thumbnail. The thumbnails will be links as
+		well, so interested parties can visit sites that I've made in the past quickly and conveniently.
+		</p>
+		<p>
+			<h3>Contact us: </h3> This section will have a simple form to fill out that will put the user in contact with me.
+		It will also display my professional email and phone number, should they wish to contact me through those avenues.
+		I will also have links to my professional social media, such as linkedIn and GitHub.
+		</p>
+		<p>
+			<h2>WireFrames: </h2>
+		<h3>Mobile site:</h3>
+		<img src="" alt="">
+		<h3>Desktop site: </h3>
+		<img src="" alt="">
+		</p>
+	</body>
+</html>
